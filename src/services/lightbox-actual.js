e76cb6741263cb01bd93d7eb85acabf114fbdd72
David Mzareulyan <david@hiero.ru>
2025-02-14 23:34:29 +0300

    Play/pause the current video by Space key

--- src/services/lightbox-actual.js
+++ src/services/lightbox-actual.js
@@ -147,6 +147,17 @@ function initLightbox() {
     }
   });
 
+  // Looking for video in active slide
+  let currentVideo = null;
+  lightbox.on('contentActivate', ({ content }) => {
+    currentVideo = content.element.matches('video')
+      ? content.element
+      : content.element.querySelector('video');
+  });
+  lightbox.on('contentDeactivate', () => {
+    currentVideo = null;
+  });
+
   // Disable scrolling while lightbox is open
   const htmlCl = document.documentElement.classList;
   lightbox.on('openingAnimationStart', () => htmlCl.add('with-lightbox', 'with-lightbox-bg'));
@@ -158,11 +169,18 @@ function initLightbox() {
     Mousetrap.bind(prevHotKeys, () => lightbox.pswp.prev());
     Mousetrap.bind(nextHotKeys, () => lightbox.pswp.next());
     Mousetrap.bind(fullScreenHotKeys, () => document.querySelector('.pswp__button--fs')?.click());
+    Mousetrap.bind(['space'], () => {
+      // Toggle play/pause for active video
+      if (currentVideo) {
+        currentVideo.paused ? currentVideo.play() : currentVideo.pause();
+      }
+    });
   });
   lightbox.on('destroy', () => {
     Mousetrap.unbind(prevHotKeys);
     Mousetrap.unbind(nextHotKeys);
     Mousetrap.unbind(fullScreenHotKeys);
+    Mousetrap.unbind(['space']);
   });
 
   // Mount/unmount HTML content. This content can contain interactive players,
