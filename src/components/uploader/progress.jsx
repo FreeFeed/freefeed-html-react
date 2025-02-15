82a48666135b1868d10971cf6b9d89f88666a36c
David Mzareulyan <david@hiero.ru>
2025-02-15 16:04:41 +0300

    Show the current upload speed

--- src/components/uploader/progress.jsx
+++ src/components/uploader/progress.jsx
@@ -1,3 +1,4 @@
+import { filesize } from 'filesize';
 import cn from 'classnames';
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,6 +8,7 @@ import { ButtonLink } from '../button-link';
 import { Icon } from '../fontawesome-icons';
 import { initialAsyncState } from '../../redux/async-helpers';
 import style from './progress.module.scss';
+import { useSpeed } from './use-speed';
 
 export function UploadProgress({ uploadIds, statuses, unfinishedFiles }) {
   return (
@@ -26,9 +28,12 @@ export function UploadProgress({ uploadIds, statuses, unfinishedFiles }) {
 function ProgressRow({ id, status, file }) {
   const dispatch = useDispatch();
   const allUploads = useSelector((state) => state.attachmentUploads);
+  const upl = allUploads[id];
 
   const remove = useCallback(() => dispatch(resetAttachmentUpload(id)), [dispatch, id]);
   const retry = useCallback(() => dispatch(createAttachment(id, file)), [dispatch, id, file]);
+
+  const speed = useSpeed(status.progress * (upl?.size ?? 0));
 
   if (!status.loading && !status.error) {
     return null;
@@ -36,29 +41,28 @@ function ProgressRow({ id, status, file }) {
 
   return (
     <div className={cn(style.row, status.error && style.rowError)}>
-      <div className={style.name}>{allUploads[id]?.name ?? 'File'}</div>
+      <div className={style.name}>{upl?.name ?? 'File'}</div>
+      {status.error ? (
+        <>
+          <div className={style.error}>
+            <Icon icon={faExclamationTriangle} /> {status.errorText}
+          </div>
+          {status.errorText === 'Network error' && (
+            <button className="btn btn-xs btn-default" onClick={retry}>
+              Retry
+            </button>
+          )}
+          <ButtonLink className={style.closeIcon} onClick={remove} data-id={id}>
+            <Icon icon={faTimesCircle} />
+          </ButtonLink>
+        </>
+      ) : (
+        <div className={style.speed}>{filesize(speed, { bits: true })}/s</div>
+      )}
       <div
         className={style.progress}
         style={{ width: `${(status.error ? 1 : status.progress) * 100}%` }}
       />
-      {status.error && (
-        <>
-          <ButtonLink className={style.closeIcon} onClick={remove} data-id={id}>
-            <Icon icon={faTimesCircle} />
-          </ButtonLink>
-          <div className={style.error}>
-            <Icon icon={faExclamationTriangle} /> {status.errorText}
-            {status.errorText === 'Network error' && (
-              <>
-                &nbsp;
-                <button className="btn btn-xs btn-default" onClick={retry}>
-                  Retry
-                </button>
-              </>
-            )}
-          </div>
-        </>
-      )}
     </div>
   );
 }
